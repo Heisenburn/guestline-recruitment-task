13352b24522ca8da578531a261a279315b961055
Marcin Rudnik <marcinrudnik1996@gmail.com>
2022-10-19 10:44:47 +0200

    fix conflict

--- src/domains/homePage/components/Filters/Filters.tsx
+++ src/domains/homePage/components/Filters/Filters.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Rating, Button } from '@mui/material'
+import { Typography, Rating } from '@mui/material'
 import {
   CounterWrapper,
   FiltersWrapper,
--- src/domains/homePage/components/HotelListing/HotelListing.tsx
+++ src/domains/homePage/components/HotelListing/HotelListing.tsx
@@ -1,14 +1,11 @@
 import type { MergedHotelWithDetailsType } from '../../types/types'
 import React, { useEffect, useState } from 'react'
-import { Card, CardContent, Typography } from '@mui/material'
+import { CardContent, Typography } from '@mui/material'
 import HotelBaseInfo from '../HotelBaseInfo/HotelBaseInfo'
 import { HotelRoomsInfo } from '../HotelRoomsInfo/HotelRoomsInfo'
-<<<<<<< Updated upstream
-=======
 import { HotelListingContainer, StyledCard } from './HotelListing.theme'
 import getDataAfterFilteringByRating from '../../helpers/getDataAfterFilteringByRating'
 import { getDataAfterFilteringHotelsByOccupancy } from '../../helpers/getDataAfterFilteringHotelsByOccupancy'
->>>>>>> Stashed changes
 
 interface Props {
   data: MergedHotelWithDetailsType[]
@@ -43,30 +40,6 @@ export const HotelListing = ({
     setListingItems(hotelsMatchingOccupancyFilter)
   }, [data, numberOfAdults, numberOfChildren, selectedRating])
 
-<<<<<<< Updated upstream
-  return listingItems.length > 0 ? (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      {listingItems.map((hotel) => (
-        <Card key={hotel.id} sx={{ margin: '10px', width: '90%' }}>
-          <CardContent>
-            <HotelBaseInfo hotel={hotel} key={hotel.id} />
-            {hotel.rooms.map((room) => (
-              <HotelRoomsInfo room={room} key={room.id} />
-            ))}
-          </CardContent>
-        </Card>
-      ))}
-    </div>
-  ) : (
-    <Typography variant="h3">No results... Try changing filters</Typography>
-    //Todo: tu mógłby być button z czyszczeniem filtrów
-=======
   return (
     <HotelListingContainer>
       {
@@ -90,6 +63,5 @@ export const HotelListing = ({
         //TODO: i też czyszczenie filtrów mogłoby się pojawiać po 1szym ustawieniu filtra jakiekolwiek
       }
     </HotelListingContainer>
->>>>>>> Stashed changes
   )
 }
